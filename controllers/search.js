04a95cb06980d1251f95044512fa0b6c751d93b3
unknown <ujjgpta9@gmail.com>
2024-01-03 22:44:49 +0530

    search controller done

--- controllers/search.js
+++ controllers/search.js
@@ -2,17 +2,14 @@ const Note = require('../models/Note');
 const User = require('../models/User');
 var bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
-// add owner : req.user.id in each find
+
 const SearchController = {
 
     searchNote: async (req, res) => {
         try {
-            const notes = await User.find({ author: req.user.id });
-            const sharedWithMe = await User.find({ allowedAccess: { $contains: req.user.id } });
-            return res.json({
-                data: { notes, sharedWithMe },
-                tag: true
-            })
+            const query=req.query.q;
+            const searchResults = await Note.find({_id:req.user.id, $text: { $search: query } });
+            return res.json({tag:true,data:searchResults});
         }
         catch (err) {
             return res.json({ "message": err, "tag": false })
