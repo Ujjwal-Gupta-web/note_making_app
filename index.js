f81e9ad6bb397f01a57051f5b238f2c828a7b9f3
unknown <ujjgpta9@gmail.com>
2024-01-03 22:57:38 +0530

    index.js updates

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 const app=express();
+
 const dotenv=require('dotenv');
-
 const PORT=process.env.PORT;
+const DB=process.env.DB;
 
-app.use(express.json());
-app.use(cors());
-dotenv.config('./.env');
-
+// importing routes
 const userRoutes=require("./routes/user.route.js")
 const noteRoutes=require("./routes/note.route.js")
 const searchRoutes=require("./routes/search.route.js")
 
+// essential middlewares
+app.use(express.json());
+app.use(cors());
+
+// .env configuration
+dotenv.config();
+
+// Database connection
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("Database connected.");
+}).catch((err) => {
+    console.log("Database error");
+    console.log(err);
+});
+
+
+// routes
 app.use('/api/auth',userRoutes)
 app.use('/api/notes',noteRoutes)
 app.use('/api/search',searchRoutes)
+
 
 app.listen(PORT,()=>console.log("Server up and running"));
