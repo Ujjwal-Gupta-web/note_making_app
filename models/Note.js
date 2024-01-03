2d3c623e5c25fa1e1d016284b0ffdd0769aba99a
unknown <ujjgpta9@gmail.com>
2024-01-03 22:39:22 +0530

    note model index created

--- models/Note.js
+++ models/Note.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Note = new Schema({
+    title:{
+        type: String, 
+        required: true 
+    },
     desc: { 
         type: String, 
         required: true 
@@ -16,6 +20,7 @@ const Note = new Schema({
         ref: 'Users'
     }]
 });
+Note.index({title: 'text', desc: 'text'});
 
 
 module.exports = mongoose.model('Notes', Note); 
